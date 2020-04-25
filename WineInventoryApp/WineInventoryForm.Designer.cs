58f6ff103a34b4a1b7a0252accf7589894236435
TristanTurcotte <tristanturcotte@gmail.com>
2020-04-25 12:16:48 -0400

    Added a menu and status strip to the primary form.

---
+++ WineInventoryApp/WineInventoryForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WineInventoryApp
+{
+    partial class WineInventoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentPanel.Location = new System.Drawing.Point(0, 24);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(800, 426);
+            this.contentPanel.TabIndex = 0;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // WineInventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.contentPanel);
+            this.Controls.Add(this.menuStrip);
+            this.Name = "WineInventoryForm";
+            this.Text = "Wine Inventory";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel contentPanel;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+    }
+}
+
---
+++ WineInventoryApp/WineInventoryForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WineInventoryApp
+{
+    public partial class WineInventoryForm : Form
+    {
+        // The panel 'contentPanel' will be used as an anchor point for
+        // custom user controls to be dynamically placed and removed
+        // from the WineInventoryForm GUI.
+        // 
+        // Custom user control examples:
+        // Login page
+        // profile page
+        // inventory page
+        // add/edit wine page
+        // add/remove from inventory page
+        // orders page, etc.
+        //
+        // The navigation between these pages can be tracked using
+        // an ordered collection that holds the pages, if the user 
+        // goes 'back' the control at a given index in the collection
+        // would get displayed.
+        //
+        // WineInventoryForm has a menu bar, 'menuStrip' which can be
+        // given different options depending on which page is open.
+        // There is also a status bar, 'statusStrip' which can have a
+        // progress bar or label to display what is currently happening
+        // on the back-end, ex. Loading database query, saving database,
+        // etc.
+
+        public WineInventoryForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
