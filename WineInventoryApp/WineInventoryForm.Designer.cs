ca13c3d83e67f04b5993aff6eb5436935c53b2df
xYanchi <yanchitheone@gmail.com>
2020-04-25 23:02:51 -0400

    User Control Account Mgmt Page added

--- WineInventoryApp/WineInventoryForm.Designer.cs
+++ WineInventoryApp/WineInventoryForm.Designer.cs
@@ -30,13 +30,18 @@
         {
             this.contentPanel = new System.Windows.Forms.Panel();
             this.menuStrip = new System.Windows.Forms.MenuStrip();
-            this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.accountMgmtPage1 = new WineInventoryApp.Controls.Pages.AccountMgmtPage();
+            this.accountMgmtPage = new WineInventoryApp.Controls.Pages.AccountMgmtPage();
+            this.contentPanel.SuspendLayout();
             this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // contentPanel
             // 
+            this.contentPanel.Controls.Add(this.accountMgmtPage);
+            this.contentPanel.Controls.Add(this.accountMgmtPage1);
             this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.contentPanel.Location = new System.Drawing.Point(0, 24);
             this.contentPanel.Name = "contentPanel";
@@ -53,6 +58,13 @@
             this.menuStrip.TabIndex = 1;
             this.menuStrip.Text = "menuStrip1";
             // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
             // statusStrip
             // 
             this.statusStrip.Location = new System.Drawing.Point(0, 428);
@@ -61,12 +73,22 @@
             this.statusStrip.TabIndex = 2;
             this.statusStrip.Text = "statusStrip1";
             // 
-            // fileToolStripMenuItem
+            // accountMgmtPage1
             // 
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "&File";
+            this.accountMgmtPage1.Location = new System.Drawing.Point(0, 0);
+            this.accountMgmtPage1.MinimumSize = new System.Drawing.Size(800, 426);
+            this.accountMgmtPage1.Name = "accountMgmtPage1";
+            this.accountMgmtPage1.Size = new System.Drawing.Size(800, 426);
+            this.accountMgmtPage1.TabIndex = 0;
+            // 
+            // accountMgmtPage
+            // 
+            this.accountMgmtPage.BackColor = System.Drawing.SystemColors.Window;
+            this.accountMgmtPage.Location = new System.Drawing.Point(0, 0);
+            this.accountMgmtPage.MinimumSize = new System.Drawing.Size(800, 426);
+            this.accountMgmtPage.Name = "accountMgmtPage";
+            this.accountMgmtPage.Size = new System.Drawing.Size(800, 426);
+            this.accountMgmtPage.TabIndex = 1;
             // 
             // WineInventoryForm
             // 
@@ -78,6 +100,7 @@
             this.Controls.Add(this.menuStrip);
             this.Name = "WineInventoryForm";
             this.Text = "Wine Inventory";
+            this.contentPanel.ResumeLayout(false);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -91,6 +114,8 @@
         private System.Windows.Forms.MenuStrip menuStrip;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip;
+        private Controls.Pages.AccountMgmtPage accountMgmtPage1;
+        private Controls.Pages.AccountMgmtPage accountMgmtPage;
     }
 }
 
