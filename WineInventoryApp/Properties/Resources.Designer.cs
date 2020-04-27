88ec85763825b7c4cfc45f902ded89857787f82d
TristanTurcotte <tristanturcotte@gmail.com>
2020-04-27 01:42:07 -0400

    Readded binary image files.

--- WineInventoryApp/Properties/Resources.Designer.cs
+++ WineInventoryApp/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace WineInventoryApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap minus {
+            get {
+                object obj = ResourceManager.GetObject("minus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap plus {
+            get {
+                object obj = ResourceManager.GetObject("plus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap RestaurantName {
             get {
                 object obj = ResourceManager.GetObject("RestaurantName", resourceCulture);
