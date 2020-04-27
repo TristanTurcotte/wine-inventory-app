cf69ac42442ae89edb410d980fff7358d0c9c782
TristanTurcotte <tristanturcotte@gmail.com>
2020-04-27 12:45:33 -0400

    Added documentation to the data object classes.

--- WineInventoryApp/Data/User.cs
+++ WineInventoryApp/Data/User.cs
@@ -1,19 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WineInventoryApp.Data
 {
+    /// <summary>
+    /// Structure which represents the data in the row of the User table.
+    /// Each row representing a user's account information, a username,
+    /// application access level, account creation date, and last login
+    /// time.
+    /// </summary>
     class User
     {
+        /// <summary>
+        /// Primary key user account identifier. Auto-increments by 1.
+        /// </summary>
         int UserId { get; }
+
+        /// <summary>
+        /// User account's username for a human readable identifier.
+        /// </summary>
         string Username { get; set; }
+
+        /// <summary>
+        /// Access level to various features of the application. The 3 levels being:
+        /// Change quantity from Inventory(0), edit and add wine(1), full admin
+        /// rights(2).
+        /// </summary>
         int AccessLevel { get; set; }
+
+        /// <summary>
+        /// Date this user account was created.
+        /// </summary>
         DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Last time this user logged into the application.
+        /// </summary>
         DateTime LastLoginTime { get; set; }
 
+        /// <summary>
+        /// Standard constructor. Used by AppDatabase class to provide a typed
+        /// structure to represent a user account.
+        /// </summary>
+        /// <param name="id">Primary key account identifier.</param>
+        /// <param name="name">Account username.</param>
+        /// <param name="access">Application access rights.</param>
+        /// <param name="created">User account creation date.</param>
+        /// <param name="lastLogin">Time and date of last login.</param>
         public User(int id, string name, int access, DateTime created, DateTime lastLogin)
         {
             UserId = id;
@@ -22,11 +54,5 @@ namespace WineInventoryApp.Data
             CreatedDate = created;
             LastLoginTime = lastLogin;
         }
-
-
-
-
-
-
     }
 }
--- WineInventoryApp/Data/Wine.cs
+++ WineInventoryApp/Data/Wine.cs
@@ -1,22 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WineInventoryApp.Data
+﻿namespace WineInventoryApp.Data
 {
+    /// <summary>
+    /// Structure which represents the data in the row of the Wine table.
+    /// Each row representing a bottle of wine, containing a numerical id,
+    /// a name, country of origin, price, year vintaged, volume of a
+    /// bottle, category or type of wine, and an image.
+    /// </summary>
     class Wine
     {
+        /// <summary>
+        /// Primary key for this wine 
+        /// </summary>
         int WineId { get; set; }
+        
+        /// <summary>
+        /// The name of the wine.
+        /// </summary>
         string WineName { get; set; }
+
+        /// <summary>
+        /// This wine's origin country or region.
+        /// </summary>
         string Origin { get; set; }
+
+        /// <summary>
+        /// Price for one bottle of this wine.
+        /// </summary>
         decimal Price { get; set; }
+
+        /// <summary>
+        /// The vintage of this wine.
+        /// </summary>
         int Year { get; set; }
+
+        /// <summary>
+        /// Volume of a bottle of this wine.
+        /// </summary>
         int Volume { get; set; }
+
+        /// <summary>
+        /// Type of wine: Red, White, Sparkling, etc.
+        /// </summary>
         string Type { get; set; }
+
+        /// <summary>
+        /// Array of bytes representing an image.
+        /// </summary>
         byte[] Image { get; set; }
 
+        /// <summary>
+        /// Standard constructor. Used by AppDatabase class to provide a typed
+        /// structure to represent wine.
+        /// </summary>
+        /// <param name="wineId">Primary key.</param>
+        /// <param name="wineName">Name of this wine.</param>
+        /// <param name="origin">Country or region of the wine.</param>
+        /// <param name="price">Price for a bottle.</param>
+        /// <param name="year">Year vintaged.</param>
+        /// <param name="volume">Volume of one bottle.</param>
+        /// <param name="type">Type of wine.</param>
+        /// <param name="image">Image of the wine bottle.</param>
         public Wine(int wineId, string wineName, string origin, decimal price, int year, int volume, string type, byte[] image)
         {
             WineId = wineId;
