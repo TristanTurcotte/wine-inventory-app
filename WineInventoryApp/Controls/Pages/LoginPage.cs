816a68864493b7776c222e36468e26468cacc146
TristanTurcotte <tristanturcotte@gmail.com>
2020-04-28 17:21:43 -0400

    Added login functionality, on login success switches to the Inventory page.

--- WineInventoryApp/Controls/Pages/LoginPage.cs
+++ WineInventoryApp/Controls/Pages/LoginPage.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WineInventoryApp.Controller;
 
 namespace WineInventoryApp.Controls.Pages
 {
@@ -16,6 +17,32 @@ namespace WineInventoryApp.Controls.Pages
         public LoginPage()
         {
             InitializeComponent();
+
+            int userId = Accounts.CreateUser("testadmin", "123321", 3);
+            System.Diagnostics.Debug.WriteLine("Was is successful? {0}", userId);
+        }
+
+        private void SetInvalidLabel(bool visible)
+        {
+            invalidEntryLabel.Visible = visible;
+        }
+
+        private void signInButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            bool validLogin = Accounts.TryLogin(username, password);
+            if(!validLogin)
+            {
+                SetInvalidLabel(true);
+                return;
+            }
+            
+            Navigator nav = Navigator.GetNavigator();
+            nav.NavigateForward(new InventoryPage());
+            nav.SetNavigationPanelVisible(true);
+            nav.PurgeNavigationHistory();
         }
     }
 }
